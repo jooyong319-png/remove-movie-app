b77d6f92705804c5360921b93273f28a1514918f
jooyong319-png <jooyong319@gmail.com>
2026-02-11 17:41:57 +0900

    Create project

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { Component } from './core/heropy'
+import TheHeader from "./components/TheHeader";
+import TheFooter from './components/TheFooter';
+import TheFooter from './components/TheFooter';
+
+
+export default class App extends Component {
+    render() {
+        const theHeader = new TheHeader().el
+        const routerView = document.createElement('router-view')
+        const theFooter = new TheFooter().el
+
+        this.el.append(
+            theHeader,
+            routerView,
+            theFooter
+        )
+    }
+}
---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+import { Store } from "../core/heropy";
+
+export default new Store({
+    photo: 'https://blogpfthumb-phinf.pstatic.net/MjAyNTA0MDZfMjUz/MDAxNzQzODY3NTA4Njk5.5LB-oiFEIbFXoDeGddlcW9eIgYFTL22dTIMJT3sbkV0g.PGVBKBkiNrbZm2-YZAbxPjbI2oSo9i-UORB_rsGI_YAg.JPEG/%EB%94%B8%EA%B8%B0-%ED%94%84%EB%A1%9C%ED%95%84-002.jpg/%25EB%2594%25B8%25EA%25B8%25B0-%25ED%2594%2584%25EB%25A1%259C%25ED%2595%2584-002.jpg?type=w161',
+    name: 'J-dragon / KimJuYong',
+    email: 'jooyong319@gmail.com',
+    blog: 'https://blog.naver.com/j-dragon_',
+    github: 'https://github.com/jooyong319-png',
+    repository: 'https://github.com/jooyong319-png/Movie'
+})
