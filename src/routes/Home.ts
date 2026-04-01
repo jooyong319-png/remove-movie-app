b2222ba40ddacd0948f8d4e9e9fb38779f0e8717
jooyong319-png <jooyong319@gmail.com>
2026-04-01 15:53:08 +0900

    Open AI 챗봇 기능 추가

--- src/routes/Home.ts
+++ src/routes/Home.ts
@@ -3,6 +3,8 @@ import Headline from '../components/Headline'
 import Search from '../components/Search'
 import MovieList from '../components/MovieList'
 import MovieListMore from '../components/MovieListMore'
+import Chatbot from '../components/Chatbot'
+import chatbot from '../components/Chatbot'
 
 export default class Home extends Component {
   render() {
@@ -10,13 +12,15 @@ export default class Home extends Component {
     const search = new Search().el
     const movieList = new MovieList().el
     const movieListMore = new MovieListMore().el
+    const Chatbot = new chatbot().el
     
     this.el.classList.add('container')
     this.el.append(
       headline,
       search,
       movieList,
-      movieListMore
+      movieListMore,
+      Chatbot
     )
   }
 }
